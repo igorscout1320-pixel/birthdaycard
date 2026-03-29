82bb70724e5e141154e296cf25c5ebf6257eaeae
igorscout1320-pixel <igorscout1320@gmail.com>
2026-03-29 14:26:11 +0900

    Add files via upload

---
+++ BirthdayCard/src/main/java/birthdaycardapp/birthdaycard/BirthdayCardApplication.java
@@ -0,0 +1,13 @@
+package birthdaycardapp.birthdaycard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirthdayCardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BirthdayCardApplication.class, args);
+    }
+
+}
